702187b172ca711b7ac582e14c29039dd4db54d7
Krishna Kant <krishna.200587@gmail.com>
2020-10-01 13:25:41 +0530

    back end app

---
+++ src/main/java/io/github/krishkant/employeeapp/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package io.github.krishkant.employeeapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.krishkant.employeeapp.model.Employee;
+import io.github.krishkant.employeeapp.repository.EmployeeRepository;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployee() {
+        return employeeRepository.findAll();
+    }
+
+}
---
+++ src/main/java/io/github/krishkant/employeeapp/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package io.github.krishkant.employeeapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import io.github.krishkant.employeeapp.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
