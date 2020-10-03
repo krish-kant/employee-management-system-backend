dccdb7f73ad5cec965576142eb59c188cb875050
Krishna Kant <krishna.200587@gmail.com>
2020-10-03 15:18:44 +0530

    delete employee controller

--- src/main/java/io/github/krishkant/employeeapp/controller/EmployeeController.java
+++ src/main/java/io/github/krishkant/employeeapp/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package io.github.krishkant.employeeapp.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,6 +64,19 @@ public class EmployeeController {
         Employee updatedEmployee = employeeRepository.save(employee);
 
         return ResponseEntity.ok(updatedEmployee);
+    }
+
+    // delete employee
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) {
+
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with id: " + id));
+
+        employeeRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("Deleted", true);
+        return ResponseEntity.ok(response);
     }
 
 }
